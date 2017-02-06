a4fb02c04154154474da800b7fb41c2645327423
Thomas Kim <thomaskim@Thomass-MacBook-Pro.local>
2017-02-06 17:46:45 +0100

    Homepage and About us complete

---
+++ js/functions.js
@@ -0,0 +1,40 @@
+(function($) {
+
+    "use strict"; // Start of use strict
+	$(document).ready(function(){
+		$("#MilanButton").click(function(){
+			$("#GraceText").hide();
+			$("#GraceImg").hide();
+			$("#YungmiText").hide();
+			$("#YungmiImg").hide();
+			$("#MilanText").show();
+			$("#MilanImg").show();
+		});
+
+	});
+
+	$(document).ready(function(){
+		$("#GraceButton").click(function(){
+			$("#MilanText").hide();
+			$("#MilanImg").hide();
+			$("#YungmiText").hide();
+			$("#YungmiImg").hide();
+			$("#GraceText").show();
+			$("#GraceImg").show();
+		});
+
+	});
+
+	$(document).ready(function(){
+		$("#YungmiButton").click(function(){
+			$("#GraceText").hide();
+			$("#GraceImg").hide();
+			$("#MilanText").hide();
+			$("#MilanImg").hide();
+			$("#YungmiText").show();
+			$("#YungmiImg").show();
+		});
+
+	});
+
+})(jQuery); // End of use strict
